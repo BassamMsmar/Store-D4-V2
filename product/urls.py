dc7bbe212c75ae13c971dac673ade4f86acd5e81
BassamMsmar <bassammsmar@gmail.com>
2023-10-10 13:06:30 +0300

    add product and brand template , views

---
+++ product/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ProductList, ProductDetail, BrandList, BrandDetail
+
+urlpatterns = [
+    path('',ProductList.as_view()),
+    path('/<int:pk>',ProductDetail.as_view()),
+
+    path('/brand',BrandList.as_view()),
+    path('/brand/<int:pk>',BrandDetail.as_view()),
+]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
+from .models import Product, Brand, Review
 # Create your views here.
+
+class ProductList(ListView):
+    model = Product
+
+class ProductDetail(DetailView):
+    model =Product
+
+
+class BrandList(ListView):
+    model = Brand
+
+class BrandDetail(DetailView):
+    model = Brand
