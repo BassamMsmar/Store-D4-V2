177ff47a579c64dbed44fbbc302f23c44ed73d39
bassammsmar <bassammsmar@gmail.com>
2023-10-13 00:02:39 +0300

    add product template and detail product

--- product/models.py
+++ product/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 from django.utils import timezone
-# from tagging.managers import TaggableManager
+# from tagget.managers import TaggableManager
 
 FLAG_TYPES = (
     ('Sale','Sale'),
