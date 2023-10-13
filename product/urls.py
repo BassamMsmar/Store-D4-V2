a0bbc08ca4676be09d77006c3923c5eb9a714420
bassammsmar <bassammsmar@gmail.com>
2023-10-13 15:09:38 +0300

    add brand detail by Detail views

--- product/urls.py
+++ product/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .views import ProductList, ProductDetail, BrandList, BrandDetail
 
 urlpatterns = [
-    path('',ProductList.as_view()),
-    path('<slug:slug>',ProductDetail.as_view()),
+    path('', ProductList.as_view(), name='product_list'),
+    path('<slug:slug>', ProductDetail.as_view(), name='product_detail'),
 
-    path('brand',BrandList.as_view()),
-    path('brand/<slug:slug>',BrandDetail.as_view()),
+    path('brands/', BrandList.as_view(), name='brand_list'), 
+    path('brands/<slug:slug>', BrandDetail.as_view(), name='brand_details'), 
+
 ]
--- product/views.py
+++ product/views.py
@@ -11,16 +11,26 @@ class ProductDetail(DetailView):
     model =Product
 
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["reviews"] = Review.objects.filter(product=self.get_object())
-        context["related_products"] = Product.objects.filter(brand=self.get_object().brand)
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["reviews"] = Review.objects.filter(product=self.get_object())
+    #     context["related_products"] = Product.objects.filter(brand=self.get_object().brand)
+    #     return context
     
 
-
 class BrandList(ListView):
-    model = Brand
+    model = Brand    #context : object_list, model_list
+    paginate_by = 20
+
+
+
+
 
 class BrandDetail(DetailView):
     model = Brand
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["product_list"] = Product.objects.all().filter(brand=self.get_object())
+        return context
+    
