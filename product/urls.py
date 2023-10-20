e8f4a731de524335050943caa4a0c7eaaf1f31d7
bassammsmar <bassammsmar@gmail.com>
2023-10-20 19:33:19 +0300

    add brand count

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProductList, ProductDetail, BrandList, BrandDetail
-
+from .views import ProductList, ProductDetail, BrandList, BrandDetail, queryset_debug
 urlpatterns = [
     path('', ProductList.as_view(), name='product_list'),
     path('<slug:slug>', ProductDetail.as_view(), name='product_detail'),
+
+    path('debug', queryset_debug, name='queryset_debug'),
 
     path('brands/', BrandList.as_view(), name='brand_list'), 
     path('brands/<slug:slug>', BrandDetail.as_view(), name='brand_details'), 
--- product/views.py
+++ product/views.py
@@ -1,28 +1,41 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.db.models import Count
 
 from .models import Product, Brand, Review, ProductImages
 # Create your views here.
 
+
 class ProductList(ListView):
     model = Product
+    queryset = Product.objects.annotate(product_reviews=Count('review_product'))
     paginate_by = 20
+
+
+
+
 
 class ProductDetail(DetailView):
     model =Product
 
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context["reviews"] = Review.objects.filter(product=self.get_object())
-    #     context["related_products"] = Product.objects.filter(brand=self.get_object().brand)
-    #     return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["reviews"] = Review.objects.filter(product=self.get_object())
+        context["related_products"] = Product.objects.filter(brand=self.get_object().brand)
+        return context
     
+
+
+
+
+
 
 class BrandList(ListView):
     model = Brand    #context : object_list, model_list
     paginate_by = 20
-
+    queryset = Brand.objects.annotate(product_count=Count('product_brand'))
+ 
 
 
 
@@ -34,11 +47,39 @@ class BrandDetail(ListView):
 
     def get_queryset(self):
         brand = Brand.objects.get(slug=self.kwargs['slug'])
-        return super().get_queryset().filter(brand=brand)
+        return super().get_queryset().filter(brand=brand) 
     
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["brand"] = Brand.objects.get(slug=self.kwargs['slug'])
+        context["brand"] = Brand.objects.annotate(product_count=Count('product_brand')).get(slug=self.kwargs['slug'])
         return context
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def queryset_debug(request):
+    # products = Product.objects.select_related('brand').all()
+    products = Product.objects.filter(brand__name='Apple')
+
+    return render(request, 'product/debug.html', {'products':products})
