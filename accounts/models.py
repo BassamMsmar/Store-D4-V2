958d37634e77db3830001316093a34abb814d1ee
Bassam Msmar <bassammsmar@gmail.com>
2024-03-08 10:33:47 +0300

    add create_profile signal

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from utils.generate_code import generate_code
 
@@ -11,7 +13,7 @@ Permissions = (
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
     code = models.CharField(max_length=9, default=generate_code())
     phone = models.CharField(max_length=16, null=True, blank=True)
     permission = models.CharField(max_length=5, choices=Permissions, default='User')
@@ -20,3 +22,10 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+post_save.connect(create_profile, sender=User)
