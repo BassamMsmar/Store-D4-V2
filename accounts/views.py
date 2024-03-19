5a11f444df0531b39a4cf11638fdbcf790cb3485
Bassam Msmar <bassammsmar@gmail.com>
2024-03-19 16:49:38 +0300

    form login

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
+
+from .forms import SignupForm
 from .models import Profile
 
 # Create your views here.
@@ -9,6 +11,13 @@ class Profile(DetailView):
     template_name = 'accounts/profile.html'
 
 def register(request):
-    return render(request, 'registration/register.html')
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
 
+    else:   
+        form = SignupForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def activate(request):
+    return render(request, 'registration/active.html')
                                                                       
