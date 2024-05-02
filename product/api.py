1e177b25617a3114749e9d1526f58a0f7a0c02ea
Bassam Msmar <bassammsmar@gmail.com>
2024-05-02 11:18:49 +0300

    Added serializers for Brand, Categories, and Product with methods to calculate average rate and reviews count

--- product/api.py
+++ product/api.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics
-from .serializers import ProductSerializer, BrandSerializer, CategoriesSerializer
+from .serializers import ProductListSerializer, ProductDetailsSerializer, BrandListSerializer, BrandDetailsSerializer, CategoriesListSerializer, CategoriesDetailsSerializer
 
 from .models import Product, Review, Brand, Categories
 
@@ -9,26 +9,26 @@ from .models import Product, Review, Brand, Categories
 
 class ProductListApi(generics.ListCreateAPIView):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    serializer_class = ProductListSerializer
 
 class ProductDetailApi(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    serializer_class = ProductDetailsSerializer
 
 
 class BrandListApi(generics.ListCreateAPIView):
     queryset = Brand.objects.all()
-    serializer_class = BrandSerializer
+    serializer_class = BrandListSerializer
 
 class BrandDetailApi(generics.RetrieveUpdateDestroyAPIView):
     queryset = Brand.objects.all()
-    serializer_class = BrandSerializer
+    serializer_class = BrandDetailsSerializer
 
 
 class CategoriesListApi(generics.ListCreateAPIView):
     queryset = Categories.objects.all()
-    serializer_class = CategoriesSerializer
+    serializer_class = CategoriesListSerializer
 
 class CategoriesDetailApi(generics.RetrieveUpdateDestroyAPIView):
     queryset = Categories.objects.all()
-    serializer_class = CategoriesSerializer
+    serializer_class = CategoriesDetailsSerializer
--- product/serializers.py
+++ product/serializers.py
@@ -1,21 +1,72 @@
 from rest_framework import serializers
+from django.db.models.aggregates import Avg
+
 from .models import Product, Brand, Categories
 
-class ProductSerializer(serializers.ModelSerializer):
+
+
+class BrandListSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Brand
+        fields = '__all__'
+
+
+class CategoriesListSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Categories
+        fields = '__all__'
+
+
+class ProductListSerializer(serializers.ModelSerializer):
+    avg_rate = serializers.SerializerMethodField()
+    reviews_count = serializers.SerializerMethodField()
+    brand = serializers.StringRelatedField()
+    categories = serializers.StringRelatedField()
 
     class Meta: 
         model = Product
         fields = '__all__'
 
+    def get_avg_rate(self, product):
+        avg = product.review_product.aggregate(rate_ave=Avg('rate'))
+        if avg['rate_ave'] is None:
+            return 0
+        return avg['rate_ave']
 
-class BrandSerializer(serializers.ModelSerializer):
+    def get_reviews_count(self, product):
+        return product.review_product.count()
+    
+
+  
+class ProductDetailsSerializer(serializers.ModelSerializer):
+
+    class Meta: 
+        model = Product
+        fields = '__all__'
+
+    
+
+class BrandListSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Brand
+        fields = '__all__'
+
+
+class BrandDetailsSerializer(serializers.ModelSerializer):
+    product = ProductListSerializer(source='product_brand', many=True)
 
     class Meta: 
         model = Brand
         fields = '__all__'
 
 
-class CategoriesSerializer(serializers.ModelSerializer):
+class CategoriesListSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Categories
+        fields = '__all__'
+
+class CategoriesDetailsSerializer(serializers.ModelSerializer):
+    product = ProductListSerializer(source='product_categories', many=True)
 
     class Meta: 
         model = Categories
