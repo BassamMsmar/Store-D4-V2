b8ab0be8c73a9208ed75c34df337fb9d961c8a53
Bassam Msmar <bassammsmar@gmail.com>
2024-05-20 18:23:01 +0300

    add product to cart api

--- orders/api.py
+++ orders/api.py
@@ -6,7 +6,7 @@ from .serializers import CartDetailSerializer, CartSerializer
 
 
 from .models import Cart , CartDetail , Order , OrderDetail
-
+from product.models import Product
 class CartCreateUpdateDelete(generics.GenericAPIView):
 
     def get(self, request, *args, **kwargs):
@@ -14,6 +14,26 @@ class CartCreateUpdateDelete(generics.GenericAPIView):
         cart, created = Cart.objects.get_or_create(user=user, status='InProgress')
         date = CartSerializer(cart).data
         return Response(date)
+    
+    def post(self,request,*args, **kwargs):
+        user = User.objects.get(username=self.kwargs['username'])
+        product = Product.objects.get(id=request.data['product_id'])
+        quantity = int(request.data['quantity'])
+
+        cart, created = Cart.objects.get_or_create(user=user, status='InProgress')
+        cart_detail , created = CartDetail.objects.get_or_create(cart=cart, product=product)
+
+        cart_detail.quantity = quantity
+        cart_detail.total = round(product.price * cart_detail.quantity,2)
+        cart_detail.save()
+
+        data = CartSerializer(cart).data
+
+        return Response({'message':'cart was updated successfully', 'data':data})
+
+
+
+        
     
 
     def delete(self, request, *args, **kwargs):
